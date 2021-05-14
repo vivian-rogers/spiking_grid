fe0d065ee25ad2a069ed5d2af44cf9c3099c6eda
vivian-rogers <43078448+vivian-rogers@users.noreply.github.com>
2021-05-13 22:12:46 -0500

    Update makegrid.cpp

--- makegrid.cpp
+++ makegrid.cpp
@@ -1,5 +1,4 @@
-// FILE: card_demo.cpp
-// This is a small demonstration program showing how the Card and Deck classes are used.
+
 #include <iostream>    // Provides cout and cin
 #include <cstdlib>     // Provides EXIT_SUCCESS
 #include <fstream>
